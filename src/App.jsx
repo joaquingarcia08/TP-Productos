2ec10091bf54fd28d4e0aa87775b48715b1bd366
Joaquin Garcia <89877889+joaquingarcia08@users.noreply.github.com>
2024-11-16 18:58:57 -0300

    arreglo 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import Home from './pages/Home.jsx'
 import Catalog from './pages/Catalog.jsx'
 import Manufacturers from './pages/Manufacturers.jsx'
-import ProductDetail from './pages/Manufacturers.jsx'
+import ProductDetail from './pages/ProductDetail.jsx'
+import { Route, Routes } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogo" element={<Catalog />} />
+        <Route path="/fabricantes" element={<Manufacturers />} />
+        <Route path="/producto/:id" element={<ProductDetail />} />
+      </Routes>
     </div>
   )
 }
